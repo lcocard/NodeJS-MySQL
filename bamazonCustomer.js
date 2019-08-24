206bcb56858c826b44f4bcf7bc1bc5bce60ed3a9
Lawrence Cocard <12580746+lcocard@users.noreply.github.com>
2019-08-24 04:46:49 -0400

    query db for item and qty then display the total cost and display the updated products table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: "localhost",
 
     // Your port;
-    port: 3300,
+    port: 3306,
 
     // Your username
     user: "root",
@@ -20,69 +20,67 @@ var inquirer = require("inquirer");
 
 /* CLI Table *************** */
 // Load the NPM Package cli-table
-var table = require("cli-table");
+var Table = require("cli-table");
 
-// Create a "Prompt" with a series of questions.
-inquirer
-    .prompt([
-        // Here we create a basic text prompt.
-        {
-            type: "input",
-            message: "What is your phone number?",
-            name: "username"
-        },
-        // Here we create a basic password-protected text prompt.
-        {
-            type: "password",
-            message: "Set your pin",
-            name: "pin"
-        },
-        // Here we give the user a list to choose from.
-        {
-            type: "list",
-            message: "What phone model you have?",
-            choices: ["iPhone", "Samsung", "Nokia"],
-            name: "phone"
-        },
+/* ************************************************************************************** */
+/* ******************* Display all products using CLI Table ***************************** */
 
-        {
-            type: 'checkbox',
-            message: "What OS?",
-            choices: ["Android", "iOS"],
-            name: "OS"
-        },
-
-        // Here we ask the user to confirm.
-        {
-            type: "confirm",
-            message: "Are you sure:",
-            name: "confirm",
-            default: true
-        }
-    ])
-    .then(function (inquirerResponse) {
-        // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-        if (inquirerResponse.confirm) {
-            console.log("\nThank you ");
-            console.log("Your " + inquirerResponse.phone + "phone number" + inquirerResponse.username + " phone has been activated!\n");
-        }
-        else {
-            console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
-        }
-    });
-
-/* SQL *********** */
-
-connection.connect(function (err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    afterConnection();
-});
-
-function afterConnection() {
-    connection.query("SELECT * FROM songs", function (err, res) {
+var ShowAllProducts = function () {
+    var query = "SELECT * FROM products";
+    connection.query(query, function (err, res) {
         if (err) throw err;
-        console.log(res);
-        connection.end();
+        var AllProductsTable = new Table({
+            head: ["Item ID", "Product Name", "Department Name", "Price", "Stock Quantity"],
+            colWidths: [11, 30, 30, 12, 20]
+        });
+        for (var i = 0; i < res.length; i++) {
+            AllProductsTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(AllProductsTable.toString());
+        Buy_Prompt();
     });
 }
+
+// Create a "Prompt" with a series of questions.
+function Buy_Prompt() {
+    inquirer.prompt([
+        // Here we create an input text prompt for getting the ITEM ID.
+        {
+            type: "input",
+            message: "Please enter the ID of the product you would like to buy.",
+            name: "ITEM_ID",
+            filter: Number
+        },
+        // Here we create an input text prompt for getting the quantity the user wants to buy.
+        {
+            type: "input",
+            message: "How many items would you like to buy?",
+            name: "BUY_QUANTITY",
+            filter: Number
+        },
+    ]).then(function (inquirerResponse) {
+        var buyQuantity = inquirerResponse.BUY_QUANTITY;
+        var item = inquirerResponse.ITEM_ID;
+        customerOrder(item, buyQuantity);
+    });
+};
+
+// Query the bamazon database, confirm if the item is available in stock and then display the total cost.
+function customerOrder(item, qty) {
+    connection.query("Select * FROM products WHERE item_id =?", [item], function (err, res) {
+        if (err) { console.log(err) };
+        if (qty <= res[0].stock_quantity) {
+            var totalCost = res[0].price * qty;
+            var updatedStockQuantity = res[0].stock_quantity - qty;
+            console.log("The item is available in stock!");
+            console.log("Total purchase cost for " + qty + " " + res[0].product_name + " = " + "CAD " + totalCost);
+            connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?", [updatedStockQuantity, item]);
+        } else {
+            console.log("Items available in stock for : " + res[0].product_name + " = " + res[0].stock_quantity);
+        };
+        ShowAllProducts();
+    });
+};
+ShowAllProducts();
